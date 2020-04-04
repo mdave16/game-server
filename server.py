25fa8f548eb119765022a349ee9838af12a543be
mdave16 <mdave3927@gmail.com>
2020-04-04 19:09:54 +0100

    add tic tac toe game

--- server.py
+++ server.py
@@ -1,8 +1,59 @@
 from bottle import route, run, Bottle, request, response
+from enum import Enum, auto
 
 app = Bottle()
 
 games = {}
+
+
+
+class Player(Enum):
+    X = auto()
+    O = auto()
+
+    def __str__():
+        self.name
+
+
+class TicTacToePosition(Enum):
+    TOP_LEFT = auto()
+    TOP_MIDDLE = auto()
+    TOP_RIGHT = auto()
+    MIDDLE_LEFT = auto()
+    MIDDLE_MIDDLE = auto()
+    MIDDLE_RIGHT = auto()
+    BOTTOM_LEFT = auto()
+    BOTTOM_MIDDLE = auto()
+    BOTTOM_RIGHT = auto()
+
+winning_conditions = [
+    [TicTacToePosition.TOP_LEFT, TicTacToePosition.TOP_MIDDLE, TicTacToePosition.TOP_RIGHT],
+    [TicTacToePosition.MIDDLE_LEFT, TicTacToePosition.MIDDLE_MIDDLE, TicTacToePosition.MIDDLE_RIGHT],
+    [TicTacToePosition.BOTTOM_LEFT, TicTacToePosition.BOTTOM_MIDDLE, TicTacToePosition.BOTTOM_RIGHT],
+    [TicTacToePosition.TOP_LEFT, TicTacToePosition.MIDDLE_LEFT, TicTacToePosition.BOTTOM_LEFT],
+    [TicTacToePosition.TOP_MIDDLE, TicTacToePosition.MIDDLE_MIDDLE, TicTacToePosition.BOTTOM_MIDDLE],
+    [TicTacToePosition.TOP_RIGHT, TicTacToePosition.MIDDLE_RIGHT, TicTacToePosition.BOTTOM_RIGHT],
+    [TicTacToePosition.TOP_LEFT, TicTacToePosition.MIDDLE_MIDDLE, TicTacToePosition.BOTTOM_RIGHT],
+    [TicTacToePosition.TOP_RIGHT, TicTacToePosition.MIDDLE_MIDDLE, TicTacToePosition.BOTTOM_LEFT]
+    ]
+
+class TicTacToe():
+    def __init__(self):
+        self.grid = {}
+        self.winner = None
+
+    def play_move(self, player, position):
+        self.grid[position] = player
+        if self.won():
+            self.winner = player
+
+    def won(self):
+        global winning_conditions
+        for cond in winning_conditions:
+            if len(set([self.grid.get(pos, None) for pos in cond])) == 1:
+                if self.grid.get(cond[0], None) is not None:
+                    return True
+
 
 @app.get('/')
 def hello():
@@ -12,7 +63,7 @@ def hello():
 def create_game():
     global games
     id = first_unused_id(games)
-    games[id] = {}
+    games[id] = TicTacToe()
     return {'id': id}
 
 @app.get('/tic-tac-toe/<id:int>')
@@ -25,7 +76,20 @@ def get_game(id):
 
 @app.put('/tic-tac-toe/<id:int>')
 def play_move(id):
-    return {'id': id}
+    global games
+    if id not in games:
+        response.status = 404
+        return
+    game = games[id]
+    game.play_move(Player[request.json['player']], TicTacToePosition[request.json['position']])
+    if game.winner is None:
+        winner = None
+        game_over = False
+    else:
+        winner = game.winner.name
+        game_over = True
+
+    return {'id': id, 'winner': winner, 'game_over': game_over}
 
 @app.delete('/tic-tac-toe/<id:int>')
 def delete_game(id):
--- test_server.py
+++ test_server.py
@@ -1,55 +1,67 @@
-import server
+from server import app, first_unused_id, TicTacToe, TicTacToePosition, Player, winning_conditions
 from webtest import TestApp, AppError
-from pytest import raises
-
+from collections import OrderedDict
 
 def test_says_hello():
-    app = TestApp(server.app)
-    server_response = app.get('/')
+    server_response = TestApp(app).get('/')
     assert server_response.text == 'Hi!'
     assert server_response.status_code == 200
 
-
 def test_creates_games_with_incremental_ids():
-    app =  TestApp(server.app)
-    game_response = app.post('/tic-tac-toe')
+    test_app = TestApp(app)
+    game_response = test_app.post('/tic-tac-toe')
     assert game_response.json == {'id': 0}
-    assert game_response.status_code == 200
-
-    game_response = app.post('/tic-tac-toe')
+    assert game_response
+    game_response = test_app.post('/tic-tac-toe')
     assert game_response.json == {'id': 1}
     assert game_response.status_code == 200
 
 def test_can_get_game_state():
-    app =  TestApp(server.app)
-    game_response = app.get('/tic-tac-toe/0')
+    game_response = TestApp(app).get('/tic-tac-toe/0')
     assert game_response.json == {'id': 0}
     assert game_response.status_code == 200
 
 def test_can_make_move_on_game():
-    app = TestApp(server.app)
-    game_response = app.put('/tic-tac-toe/0', {'player': 'X', 'position': 'TOP_LEFT'})
+    game_response = TestApp(app).put_json('/tic-tac-toe/0', {'player': 'X', 'position': 'TOP_LEFT'})
     assert game_response.json == {'id': 0}
     assert game_response.status_code == 200
 
+def test_can_make_move_on_game():
+    test_app = TestApp(app)
+    test_app.put_json('/tic-tac-toe/0', {'player': 'X', 'position': 'TOP_LEFT'})
+    test_app.put_json('/tic-tac-toe/0', {'player': 'X', 'position': 'TOP_MIDDLE'})
+    game_response = test_app.put_json('/tic-tac-toe/0', {'player': 'X', 'position': 'TOP_RIGHT'})
+    assert game_response.json == {'id': 0, 'winner': 'X', 'game_over': True}
+    assert game_response.status_code == 200
+
 def test_can_delete_games():
-    app = TestApp(server.app)
-    game_response = app.delete('/tic-tac-toe/0')
+    game_response = TestApp(app).delete('/tic-tac-toe/0')
     assert game_response.status_code == 200
 
 def test_will_error_if_cannot_find_game_to_delete():
-    app =  TestApp(server.app)
-    game_response = app.delete('/tic-tac-toe/6', expect_errors=True)
+    game_response = TestApp(app).delete('/tic-tac-toe/6', expect_errors=True)
     assert game_response.status_code == 404
 
 def test_will_error_if_cannot_find_game():
-    app =  TestApp(server.app)
-    game_response = app.get('/tic-tac-toe/0', expect_errors=True)
+    game_response = TestApp(app).get('/tic-tac-toe/0', expect_errors=True)
     assert game_response.status_code == 404
 
-
 def test_get_first_unused_id():
-    assert server.first_unused_id({}) == 0
-    assert server.first_unused_id({0: ''}) == 1
-    assert server.first_unused_id({1: ''}) == 0
-    assert server.first_unused_id({0: '', 2: ''}) == 1
+    assert first_unused_id({}) == 0
+    assert first_unused_id({0: ''}) == 1
+    assert first_unused_id({1: ''}) == 0
+    assert first_unused_id({0: '', 2: ''}) == 1
+
+def test_tic_tac_toe():
+    t = TicTacToe()
+    assert t.grid == {}
+
+    t.play_move(Player.X, TicTacToePosition.TOP_RIGHT)
+    assert t.grid == {TicTacToePosition.TOP_RIGHT: Player.X}
+
+def test_tic_tac_toe_win_conditions():
+    global winning_conditions
+    t = TicTacToe()
+    for pos in winning_conditions[0]:
+        t.play_move(Player.X, pos)
+    assert t.winner == Player.X
